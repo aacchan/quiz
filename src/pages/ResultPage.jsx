2f6f7091f12863207f48050f7e226cd8d1cce073
ryoya0519 <e225764@cs.u-ryukyu.ac.jp>
2026-02-09 01:09:23 +0900

    create result_page

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,5 +1,17 @@
+import { Link, useLocation } from "react-router-dom"
+import { ROUTES } from "../const";
+
 export default function ResultPage() {
+  const location = useLocation();
+  const maxQuizLen = location.state.maxQuizLen
+  const correctNumLen = location.state.correctNumLen
+  
   return (
-    <div>ResultPage</div>
+    <>
+    <h1>{`結果`}</h1>
+    <p>{`あなたの正答数は...`}</p>
+    <p>{`全${maxQuizLen}問中、${correctNumLen}問正解でした!`}</p>
+    <Link to={ROUTES.QUIZ}>もう一度チャレンジしますか? </Link> 
+    </>
   )
 }
